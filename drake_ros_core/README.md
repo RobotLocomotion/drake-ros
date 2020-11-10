b11ce3e8bf75f7efe3e3ad6fa1603f0b380593f5
Shane Loretz <sloretz@osrfoundation.org>
2020-11-09 17:03:21 -0800

    Add drake_ros_core package.
    Includes Drake C++ systems for publishing and subscribing to ROS2 messages.
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

---
+++ drake_ros_core/README.md
@@ -0,0 +1,35 @@
+# Drake ROS Core
+
+This package provides abstractions to simplify the use of core ROS2 functionality from within Drake.
+It introduces ROS2 equivalents of `LcmInterfaceSystem`, `LcmPublisherSystem`, `LcmSubscriberSystem` systems in Drake.
+It is similar to this ROS1 prototype [`gizatt/drake_ros_systems`](https://github.com/gizatt/drake_ros_systems).
+
+## Building
+
+This package has been built and tested on Ubuntu Focal with ROS2 Rolling, using a Drake nightly from April 2021.
+It may work on other versions of ROS and Drake, but it hasn't been tested.
+
+To build it:
+
+1. [Install ROS Rolling](https://index.ros.org/doc/ros2/Installation/Rolling/)
+1. Source your ROS installation `. /opt/ros/rolling/setup.bash`
+1. [Download April-ish 2021 Drake binary](https://drake.mit.edu/from_binary.html)
+1. Extract the Drake binary installation, install it's prerequisites, and [use this Python virutalenv trick](https://drake.mit.edu/python_bindings.html#inside-virtualenv).
+1. Activate the drake virtual environment
+1. Build it using Colcon, or using CMake directly
+    
+    **Colcon**
+    1. Make a workspace `mkdir -p ./ws/src`
+    1. `cd ./ws/src`
+    1. Get this code `git clone https://github.com/RobotLocomotion/drake-ros.git`
+    1. `cd ..`
+    1. Build this package `colcon build --packages-select drake_ros_core`
+    
+    **CMake**
+    1. Manually set `CMAKE_PREFIX_PATH`: `export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:$AMENT_PREFIX_PATH`
+    1. Get this code `git clone https://github.com/RobotLocomotion/drake-ros.git`
+    1. `cd drake-ros/drake_ros_core`
+    1. Make a build and install folder to avoid installing to the whole system `mkdir build install`
+    1. `cd build`
+    1. Configure the project `cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/../install ..`
+    1. Build the project `make && make install`
---
+++ drake_ros_core/include/drake_ros_core/drake_ros.hpp
@@ -0,0 +1,71 @@
+// Copyright 2020 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#ifndef DRAKE_ROS_CORE__DRAKE_ROS_HPP_
+#define DRAKE_ROS_CORE__DRAKE_ROS_HPP_
+
+#include <rosidl_runtime_c/message_type_support_struct.h>
+#include <rclcpp/clock.hpp>
+#include <rclcpp/node_options.hpp>
+#include <rclcpp/qos.hpp>
+#include <rclcpp/serialized_message.hpp>
+#include <tf2_ros/transform_broadcaster.h>
+
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "drake_ros_core/drake_ros_interface.hpp"
+
+namespace drake_ros_core
+{
+/// PIMPL forward declarations
+class DrakeRosPrivate;
+class Publisher;
+class Subscription;
+
+/// System that abstracts working with ROS
+class DrakeRos final : public DrakeRosInterface
+{
+public:
+  DrakeRos()
+  : DrakeRos("DrakeRosSystems", rclcpp::NodeOptions{}) {}
+
+  DrakeRos(
+    const std::string & node_name,
+    rclcpp::NodeOptions node_options);
+
+  virtual ~DrakeRos();
+
+  std::unique_ptr<Publisher>
+  create_publisher(
+    const rosidl_message_type_support_t & ts,
+    const std::string & topic_name,
+    const rclcpp::QoS & qos) final;
+
+  std::shared_ptr<Subscription>
+  create_subscription(
+    const rosidl_message_type_support_t & ts,
+    const std::string & topic_name,
+    const rclcpp::QoS & qos,
+    std::function<void(std::shared_ptr<rclcpp::SerializedMessage>)> callback) final;
+
+  void
+  spin(
+    int timeout_millis) final;
+
+private:
+  std::unique_ptr<DrakeRosPrivate> impl_;
+};
+}  // namespace drake_ros_core
+#endif  // DRAKE_ROS_CORE__DRAKE_ROS_HPP_
