c9f392cc0b4b0229bc80ac15e9abe33b4a685311
Michel Hidalgo <michel@ekumenlabs.com>
2021-04-21 20:13:12 -0300

    Complete iiwa_manipulator example in C++ & Python.
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

---
+++ drake_ros_systems/example/iiwa_manipulator.cpp
@@ -0,0 +1,85 @@
+// Copyright 2021 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include <drake/systems/analysis/simulator.h>
+#include <drake/systems/framework/diagram_builder.h>
+
+#include <drake/examples/manipulation_station/manipulation_station.h>
+
+#include <drake_ros_systems/drake_ros.hpp>
+#include <drake_ros_systems/ros_clock_system.hpp>
+#include <drake_ros_systems/ros_interface_system.hpp>
+#include <drake_ros_systems/rviz_visualizer.hpp>
+
+#include <memory>
+#include <utility>
+
+using drake_ros_systems::DrakeRos;
+using drake_ros_systems::RosClockSystem;
+using drake_ros_systems::RosInterfaceSystem;
+using drake_ros_systems::RvizVisualizer;
+
+using drake::examples::manipulation_station::ManipulationStation;
+
+
+int main() {
+  drake::systems::DiagramBuilder<double> builder;
+
+  auto ros_interface_system =
+    builder.AddSystem<RosInterfaceSystem>(std::make_unique<DrakeRos>());
+
+  auto manipulation_station = builder.AddSystem<ManipulationStation>();
+  manipulation_station->SetupClutterClearingStation();
+  manipulation_station->Finalize();
+
+  auto clock_system = builder.AddSystem<RosClockSystem>(
+    ros_interface_system->get_ros_interface().get());
+
+  auto rviz_visualizer = builder.AddSystem<RvizVisualizer>(
+    ros_interface_system->get_ros_interface());
+
+  builder.Connect(
+    clock_system->GetOutputPort("clock"),
+    rviz_visualizer->GetInputPort("clock"));
+
+  builder.Connect(
+    manipulation_station->GetOutputPort("query_object"),
+    rviz_visualizer->GetInputPort("graph_query"));
+
+  auto diagram = builder.Build();
+  auto context = diagram->CreateDefaultContext();
+
+  auto simulator =
+    std::make_unique<drake::systems::Simulator<double>>(*diagram, std::move(context));
+  simulator->set_target_realtime_rate(1.0);
+  simulator->Initialize();
+
+  auto & simulator_context = simulator->get_mutable_context();
+
+  auto & manipulation_station_context =
+    diagram->GetMutableSubsystemContext(*manipulation_station, &simulator_context);
+
+  manipulation_station->GetInputPort("iiwa_position")
+    .FixValue(
+        &manipulation_station_context,
+        manipulation_station->GetIiwaPosition(manipulation_station_context));
+
+  manipulation_station->GetInputPort("wsg_position")
+    .FixValue(&manipulation_station_context, 0.);
+
+  while (true) {
+    simulator->AdvanceTo(simulator_context.get_time() + 0.1);
+  }
+  return 0;
+
+}
--- drake_ros_systems/example/iiwa_manipulator.py
+++ drake_ros_systems/example/iiwa_manipulator.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python
 
-import numpy as np
-
 from drake_ros_systems import RosClockSystem
 from drake_ros_systems import RosInterfaceSystem
-from drake_ros_systems import RosPublisherSystem
-from drake_ros_systems import TfBroadcasterSystem
+from drake_ros_systems import RvizVisualizer
 
-from pydrake.common import FindResourceOrThrow
-from pydrake.common.value import AbstractValue
 from pydrake.examples.manipulation_station import ManipulationStation
 from pydrake.systems.analysis import Simulator
 from pydrake.systems.framework import DiagramBuilder
-from pydrake.systems.framework import TriggerType
-from pydrake.systems.primitives import ConstantValueSource
-
-from rclpy.qos import DurabilityPolicy
-from rclpy.qos import HistoryPolicy
-from rclpy.qos import ReliabilityPolicy
-from rclpy.qos import QoSProfile
-from std_msgs.msg import String
 
 
 def main():
@@ -34,46 +21,17 @@ def main():
     clock_system = builder.AddSystem(
         RosClockSystem(ros_interface_system.get_ros_interface()))
 
-    tf_broadcaster_system = builder.AddSystem(TfBroadcasterSystem(
-        ros_interface_system.get_ros_interface(),
-        {'iiwa': ''},  # drop iiwa namespace from frame names
-        {TriggerType.kForced, TriggerType.kPeriodic},
-        0.1))
+    rviz_visualizer = builder.AddSystem(
+        RvizVisualizer(ros_interface_system.get_ros_interface()))
 
     builder.Connect(
         clock_system.GetOutputPort('clock'),
-        tf_broadcaster_system.GetInputPort('clock')
+        rviz_visualizer.GetInputPort('clock')
     )
 
     builder.Connect(
         manipulation_station.GetOutputPort('query_object'),
-        tf_broadcaster_system.GetInputPort('graph_query')
-    )
-
-    # TODO(hidmic): fetch SDF path from ManipulationStation when/if it is exposed
-    manipulator_sdf_path = FindResourceOrThrow(
-        'drake/manipulation/models/iiwa_description/iiwa7/'
-        'iiwa7_no_collision.sdf')
-    with open(manipulator_sdf_path, 'r') as f:
-        manipulator_sdf_string = f.read()
-
-    robot_description_source = builder.AddSystem(
-        ConstantValueSource(AbstractValue.Make(
-            String(data=manipulator_sdf_string))))
-
-    qos = QoSProfile(depth=1)
-    qos.history = HistoryPolicy.KEEP_LAST
-    qos.durability = DurabilityPolicy.TRANSIENT_LOCAL
-    qos.reliability = ReliabilityPolicy.RELIABLE
-
-    robot_description_publisher = builder.AddSystem(
-        RosPublisherSystem(String, '/robot_description', qos,
-                           ros_interface_system.get_ros_interface(),
-                           {TriggerType.kForced}, 0))
-
-    builder.Connect(
-        robot_description_source.get_output_port(),
-        robot_description_publisher.get_input_port()
+        rviz_visualizer.GetInputPort('graph_query')
     )
 
     diagram = builder.Build()
@@ -92,7 +50,6 @@ def main():
         manipulation_station_context, np.zeros(1))
 
     try:
-        diagram.Publish(context)
         while True:
             simulator.AdvanceTo(context.get_time() + 0.1)
     except KeyboardInterrupt:
