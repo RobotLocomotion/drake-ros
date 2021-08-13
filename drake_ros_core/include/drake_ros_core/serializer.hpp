fe6dcbaa57ecbe026dce841ab41060c87fb13e31
Michel Hidalgo <michel@ekumenlabs.com>
2021-08-13 16:54:03 -0300

    Add drake_ros_core package. (#5)
    Includes Drake C++ systems for publishing and subscribing to ROS2 messages.
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

---
+++ drake_ros_core/include/drake_ros_core/serializer.hpp
@@ -0,0 +1,75 @@
+// Copyright 2021 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#ifndef DRAKE_ROS_CORE__SERIALIZER_HPP_
+#define DRAKE_ROS_CORE__SERIALIZER_HPP_
+
+#include <drake/common/value.h>
+#include <rmw/rmw.h>
+
+#include <rclcpp/serialized_message.hpp>
+#include <rosidl_typesupport_cpp/message_type_support.hpp>
+
+#include <memory>
+
+#include "drake_ros_core/serializer_interface.hpp"
+
+
+namespace drake_ros_core
+{
+template<typename MessageT>
+class Serializer : public SerializerInterface
+{
+public:
+  rclcpp::SerializedMessage
+  serialize(const drake::AbstractValue & abstract_value) const override
+  {
+    rclcpp::SerializedMessage serialized_msg;
+    const MessageT & message = abstract_value.get_value<MessageT>();
+    const auto ret = rmw_serialize(
+      &message,
+      get_type_support(),
+      &serialized_msg.get_rcl_serialized_message());
+    if (ret != RMW_RET_OK) {
+      // TODO(sloretz) do something if serialization fails
+      (void)ret;
+    }
+    return serialized_msg;
+  }
+
+  bool
+  deserialize(
+    const rclcpp::SerializedMessage & serialized_message,
+    drake::AbstractValue & abstract_value) const override
+  {
+    const auto ret = rmw_deserialize(
+      &serialized_message.get_rcl_serialized_message(),
+      get_type_support(),
+      &abstract_value.get_mutable_value<MessageT>());
+    return ret == RMW_RET_OK;
+  }
+
+  std::unique_ptr<drake::AbstractValue>
+  create_default_value() const override
+  {
+    return std::make_unique<drake::Value<MessageT>>(MessageT());
+  }
+
+  const rosidl_message_type_support_t *
+  get_type_support() const override
+  {
+    return rosidl_typesupport_cpp::get_message_type_support_handle<MessageT>();
+  }
+};
+}  // namespace drake_ros_core
+#endif  // DRAKE_ROS_CORE__SERIALIZER_HPP_
---
+++ drake_ros_core/src/ros_subscriber_system.cpp
@@ -0,0 +1,130 @@
+// Copyright 2021 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include <drake/systems/framework/abstract_values.h>
+
+#include <memory>
+#include <mutex>
+#include <string>
+#include <utility>
+
+#include "drake_ros_core/ros_subscriber_system.hpp"
+#include "subscription.hpp"
+
+namespace drake_ros_core
+{
+// Index in AbstractState that deserialized message is stored
+const int kStateIndexMessage = 0;
+
+class RosSubscriberSystemPrivate
+{
+public:
+  void
+  handle_message(std::shared_ptr<rclcpp::SerializedMessage> callback)
+  {
+    std::lock_guard<std::mutex> message_lock(mutex_);
+    // TODO(sloretz) Queue messages here? Lcm subscriber doesn't, so maybe lost messages are ok
+    // Overwrite last message
+    msg_ = callback;
+  }
+
+  std::shared_ptr<rclcpp::SerializedMessage>
+  take_message()
+  {
+    std::lock_guard<std::mutex> message_lock(mutex_);
+    return std::move(msg_);
+  }
+
+  std::unique_ptr<SerializerInterface> serializer_;
+  // A handle to a subscription
+  std::shared_ptr<Subscription> sub_;
+  // Mutex to prevent multiple threads from modifying this class
+  std::mutex mutex_;
+  // The last received message that has not yet been put into a context.
+  std::shared_ptr<rclcpp::SerializedMessage> msg_;
+};
+
+RosSubscriberSystem::RosSubscriberSystem(
+  std::unique_ptr<SerializerInterface> & serializer,
+  const std::string & topic_name,
+  const rclcpp::QoS & qos,
+  std::shared_ptr<DrakeRosInterface> ros)
+: impl_(new RosSubscriberSystemPrivate())
+{
+  impl_->serializer_ = std::move(serializer);
+  impl_->sub_ = ros->create_subscription(
+    *impl_->serializer_->get_type_support(), topic_name, qos,
+    std::bind(&RosSubscriberSystemPrivate::handle_message, impl_.get(), std::placeholders::_1));
+
+  DeclareAbstractOutputPort(
+    [serializer{impl_->serializer_.get()}]() {return serializer->create_default_value();},
+    [](const drake::systems::Context<double> & context, drake::AbstractValue * output_value) {
+      // Transfer message from state to output port
+      output_value->SetFrom(context.get_abstract_state().get_value(kStateIndexMessage));
+    });
+
+  static_assert(kStateIndexMessage == 0, "");
+  DeclareAbstractState(*(impl_->serializer_->create_default_value()));
+}
+
+RosSubscriberSystem::~RosSubscriberSystem()
+{
+}
+
+void
+RosSubscriberSystem::DoCalcNextUpdateTime(
+  const drake::systems::Context<double> & context,
+  drake::systems::CompositeEventCollection<double> * events,
+  double * time) const
+{
+  // Vvv Copied from LcmSubscriberSystem vvv
+
+  // We do not support events other than our own message timing events.
+  LeafSystem<double>::DoCalcNextUpdateTime(context, events, time);
+  DRAKE_THROW_UNLESS(events->HasEvents() == false);
+  DRAKE_THROW_UNLESS(std::isinf(*time));
+
+  std::shared_ptr<rclcpp::SerializedMessage> message = impl_->take_message();
+
+  // Do nothing unless we have a new message.
+  if (nullptr == message.get()) {
+    return;
+  }
+
+  // Create a unrestricted event and tie the handler to the corresponding
+  // function.
+  drake::systems::UnrestrictedUpdateEvent<double>::UnrestrictedUpdateCallback
+    callback = [this, serialized_message{std::move(message)}](
+    const drake::systems::Context<double> &,
+    const drake::systems::UnrestrictedUpdateEvent<double> &,
+    drake::systems::State<double> * state)
+    {
+      // Deserialize the message and store it in the abstract state on the context
+      drake::systems::AbstractValues & abstract_state = state->get_mutable_abstract_state();
+      auto & abstract_value = abstract_state.get_mutable_value(kStateIndexMessage);
+      const bool ret = impl_->serializer_->deserialize(*serialized_message, abstract_value);
+      if (ret != RMW_RET_OK) {
+        return drake::systems::EventStatus::Failed(this, "Failed to deserialize ROS message");
+      }
+      return drake::systems::EventStatus::Succeeded();
+    };
+
+  // Schedule an update event at the current time.
+  *time = context.get_time();
+  drake::systems::EventCollection<drake::systems::UnrestrictedUpdateEvent<double>> & uu_events =
+    events->get_mutable_unrestricted_update_events();
+  uu_events.add_event(
+    std::make_unique<drake::systems::UnrestrictedUpdateEvent<double>>(
+      drake::systems::TriggerType::kTimed, callback));
+}
+}  // namespace drake_ros_core
