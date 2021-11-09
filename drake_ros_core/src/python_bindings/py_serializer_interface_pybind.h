4a9404f292f7bfd56607426297efe828086009b5
Michel Hidalgo <michel@ekumenlabs.com>
2021-11-09 15:08:42 -0300

    Add Python bindings to drake_ros_core package.
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

---
+++ drake_ros_core/src/python_bindings/py_serializer_interface_pybind.h
@@ -0,0 +1,75 @@
+// Copyright 2021 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+
+#include <algorithm>
+#include <memory>
+#include <string>
+
+#include <pybind11/eval.h>
+#include <pybind11/pybind11.h>
+
+#include "drake_ros_core/serializer_interface.h"
+
+namespace py = pybind11;
+
+namespace drake_ros_core {
+// A (de)serialization interface implementation for Python ROS messages
+// that can be overriden from Python itself.
+class PySerializerInterface : public py::wrapper<SerializerInterface> {
+ public:
+  using Base = py::wrapper<SerializerInterface>;
+
+  PySerializerInterface() : Base() {}
+
+  const rosidl_message_type_support_t* GetTypeSupport() const override {
+    auto overload = [&]() -> py::capsule {
+      PYBIND11_OVERLOAD_PURE(py::capsule, SerializerInterface, GetTypeSupport);
+    };
+    return static_cast<rosidl_message_type_support_t*>(overload());
+  }
+
+  std::unique_ptr<drake::AbstractValue> CreateDefaultValue() const override {
+    PYBIND11_OVERLOAD_PURE(std::unique_ptr<drake::AbstractValue>,
+                           SerializerInterface, CreateDefaultValue);
+  }
+
+  rclcpp::SerializedMessage Serialize(
+      const drake::AbstractValue& abstract_value) const override {
+    auto overload = [&]() -> py::bytes {
+      PYBIND11_OVERLOAD_PURE(py::bytes, SerializerInterface, Serialize,
+                             &abstract_value);
+    };
+    std::string bytes = overload();
+    rclcpp::SerializedMessage serialized_message(bytes.size());
+    rcl_serialized_message_t& rcl_serialized_message =
+        serialized_message.get_rcl_serialized_message();
+    std::copy(bytes.data(), bytes.data() + bytes.size(),
+              rcl_serialized_message.buffer);
+    rcl_serialized_message.buffer_length = bytes.size();
+    return serialized_message;
+  }
+
+  void Deserialize(const rclcpp::SerializedMessage& serialized_message,
+                   drake::AbstractValue* abstract_value) const override {
+    const rcl_serialized_message_t& rcl_serialized_message =
+        serialized_message.get_rcl_serialized_message();
+    py::bytes serialized_message_bytes(
+        reinterpret_cast<const char*>(rcl_serialized_message.buffer),
+        rcl_serialized_message.buffer_length);
+    PYBIND11_OVERLOAD_PURE(void, SerializerInterface, Deserialize,
+                           serialized_message_bytes, abstract_value);
+  }
+};
+}  // namespace drake_ros_core
---
+++ drake_ros_core/test/test_pub_sub.py
@@ -0,0 +1,82 @@
+# Copyright 2021 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pydrake.systems.analysis import Simulator
+from pydrake.systems.framework import DiagramBuilder
+
+import rclpy
+from rclpy.qos import DurabilityPolicy
+from rclpy.qos import HistoryPolicy
+from rclpy.qos import QoSProfile
+from rclpy.qos import ReliabilityPolicy
+
+from test_msgs.msg import BasicTypes
+
+from drake_ros_core import RosInterfaceSystem
+from drake_ros_core import RosPublisherSystem
+from drake_ros_core import RosSubscriberSystem
+
+
+def test_nominal_case():
+    builder = DiagramBuilder()
+
+    ros_interface_system = builder.AddSystem(RosInterfaceSystem())
+
+    qos = QoSProfile(
+        depth=10,
+        history=HistoryPolicy.KEEP_LAST,
+        reliability=ReliabilityPolicy.RELIABLE,
+        durability=DurabilityPolicy.TRANSIENT_LOCAL)
+
+    ros_publisher_system = builder.AddSystem(RosPublisherSystem.Make(
+        BasicTypes, 'out_py', qos, ros_interface_system.get_ros_interface()))
+
+    ros_subscriber_system = builder.AddSystem(RosSubscriberSystem.Make(
+        BasicTypes, 'in_py', qos, ros_interface_system.get_ros_interface()))
+
+    builder.Connect(
+        ros_subscriber_system.get_output_port(0),
+        ros_publisher_system.get_input_port(0))
+    diagram = builder.Build()
+    simulator = Simulator(diagram)
+    simulator_context = simulator.get_mutable_context()
+    simulator.set_target_realtime_rate(1.0)
+
+    rclpy.init()
+    node = rclpy.create_node('sub_to_pub_py')
+
+    # Create publisher talking to subscriber system.
+    publisher = node.create_publisher(BasicTypes, 'in_py', qos)
+
+    # Create subscription listening to publisher system
+    rx_msgs = []
+    rx_callback = (lambda msg: rx_msgs.append(msg))
+    node.create_subscription(BasicTypes, 'out_py', rx_callback, qos)
+
+    # Send messages into the drake system
+    num_msgs = 5
+    for _ in range(num_msgs):
+        publisher.publish(BasicTypes())
+
+    timeout_sec = 5
+    spins_per_sec = 10
+    time_delta = 1.0 / spins_per_sec
+    for _ in range(timeout_sec * spins_per_sec):
+        if len(rx_msgs) >= num_msgs:
+            break
+        rclpy.spin_once(node, timeout_sec=time_delta)
+        simulator.AdvanceTo(simulator_context.get_time() + time_delta)
+
+    # Make sure same number of messages got out
+    assert num_msgs == len(rx_msgs)
