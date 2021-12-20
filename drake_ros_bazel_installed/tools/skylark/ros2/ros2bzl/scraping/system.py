c8c51f0548c5089677f2d272e5f05439d8b59569
Michel Hidalgo <michel@ekumenlabs.com>
2021-12-20 16:01:33 -0300

    Improve ros2bzl.scraping.system module
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- drake_ros_bazel_installed/tools/skylark/ros2/ros2bzl/scraping/system.py
+++ drake_ros_bazel_installed/tools/skylark/ros2/ros2bzl/scraping/system.py
@@ -1,3 +1,8 @@
+"""
+Utility functions to aid compilation and linkage configuration scraping by
+resorting to (Linux) system-wide conventions and tooling.
+"""
+
 import os
 import re
 import subprocess
@@ -7,6 +12,10 @@ DEFAULT_INCLUDE_DIRECTORIES = ['/usr/include', '/usr/local/include']
 
 
 def is_system_include(include_path):
+    """
+    Checks whether `include_path` is in a system include directory
+    i.e. known to compilers.
+    """
     include_path = os.path.realpath(include_path)
     return any(include_path.startswith(path) for path in DEFAULT_INCLUDE_DIRECTORIES)
 
@@ -15,24 +24,39 @@ DEFAULT_LINK_DIRECTORIES = ['/lib', '/usr/lib', '/usr/local/lib']
 
 
 def is_system_library(library_path):
+    """
+    Checks whether `include_path` is in a system library directory
+    i.e. known to linkers.
+    """
     library_path = os.path.realpath(library_path)
     return any(library_path.startswith(path) for path in DEFAULT_LINK_DIRECTORIES)
 
 
-LD_LIBRARY_PATHS = []
-if 'LD_LIBRARY_PATH' in os.environ:
-    value = os.environ['LD_LIBRARY_PATH']
-    LD_LIBRARY_PATHS = [path for path in value.split(':') if path]
+LD_LIBRARY_PATHS = [
+    path for path in os.environ.get('LD_LIBRARY_PATH', '').split(':') if path]
 
 
-def find_library_path(library_name, link_directories=[], link_flags=[]):
+def find_library_path(library_name, link_directories=None, link_flags=None):
+    """
+    Finds the path to a library.
+
+    To do so it relies on the GNU `ld` linker and its library naming spec,
+    effectively reusing all of its search logic.
+
+    :param library_name: name of the library to be searched
+    :param link_directories: optional list of directories to search in
+    :param link_flags: optional list of linker flags to account for
+    :returns: the path to the library if found, None otherwise
+    """
     # Adapted from ctypes.util.find_library_path() implementation.
     pattern = r'/(?:[^\(\)\s]*/)*lib{}\.[^\(\)\s]*'.format(library_name)
 
     cmd = ['ld', '-t']
-    for path in link_directories:
-        cmd.extend(['-L', path])
-    cmd.extend(link_flags)
+    if link_directories:
+        for path in link_directories:
+            cmd.extend(['-L', path])
+    if link_flags:
+        cmd.extend(link_flags)
     for path in LD_LIBRARY_PATHS:
         cmd.extend(['-L', path])
     for path in DEFAULT_LINK_DIRECTORIES:
@@ -51,7 +75,7 @@ def find_library_path(library_name, link_directories=[], link_flags=[]):
             path = match.group(0)
             # Remove double forward slashes, if any
             return os.path.join('/', os.path.relpath(path, '/'))
-    except Exception as e:
+    except Exception:
         pass
     return None
 
@@ -60,6 +84,15 @@ LDD_LINE_PATTERN = re.compile(r' => (/(?:[^\(\)\s]*/)*lib[^\(\)\s]*)')
 
 
 def find_library_dependencies(library_path):
+    """
+    Lists all shared library dependencies of a given library.
+
+    To do so, it relies on the `ldd` tool as found in Linux.
+
+    :param library_path: path to the library to be inspected
+    :returns: a generator that iterates over the paths to
+      library dependencies
+    """
     try:
         lines = subprocess.run(
             ['ldd', library_path],
@@ -72,4 +105,5 @@ def find_library_dependencies(library_path):
             if match:
                 yield match.group(1)
     except Exception:
-        return
+        pass
+    return
