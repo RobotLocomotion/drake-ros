344e26e42acb0b8ae37f980e7745a807a96ab7d0
Eric Cousineau <eric.cousineau@tri.global>
2022-04-08 19:04:43 -0400

    i suck at using docker

--- repro/run_on_host.sh
+++ repro/run_on_host.sh
@@ -9,4 +9,12 @@ if [[ ! -f ${deb} ]]; then
     wget https://repo.radeon.com/amdgpu-install/21.50.2/ubuntu/focal/${deb}
 fi
 
-docker run -it -v ${PWD}/..:/opt/drake-ros ros:rolling-ros-base-focal bash -c '/opt/drake-ros/repro/run_on_container.sh'
+# Stupid gymnastics to ensure the entrypoint for this container isn't our
+# provisioning script (so it's easy to start and exec stuff).
+docker run -d -v ${PWD}/..:/opt/drake-ros ros:rolling-ros-base-focal sleep 100000
+id=$(docker ps -q | head -n 1)
+
+docker exec -it ${id} /opt/drake-ros/repro/run_on_container.sh
+
+# To clean up, can be heavy handed and remove *all* containers:
+#   docker ps -a -q | xargs docker rm -f
