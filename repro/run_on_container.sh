6dc67c38e8e79037e6b2eaa33918a1291e0b2559
Eric Cousineau <eric.cousineau@tri.global>
2022-04-08 19:05:05 -0400

    try to automate shane's suggestion; i was unable to repro the issue as he was tho

--- repro/run_on_container.sh
+++ repro/run_on_container.sh
@@ -11,9 +11,19 @@ cd ros2_example_bazel_installed
 yes | ./setup/install_prereqs.sh
 
 cd ../repro
-yes | apt install dialog
-dpkg -i ./amdgpu-install_21.50.2.50002-1_all.deb
-yes | amdgpu-install --opencl=rocr,legacy --no-32 --accept-eula --no-dkms
+
+# # Attepmting full install. Very long to download, install, etc.
+# yes | apt install dialog
+# dpkg -i ./amdgpu-install_21.50.2.50002-1_all.deb
+# yes | amdgpu-install --opencl=rocr,legacy --no-32 --accept-eula
+
+# Shane's suggestion (my attempt to automate?)
+cat > /etc/ld.so.conf.d/amdgpu-pro.conf <<EOF
+/opt/amdgpu-pro/lib/x86_64-linux-gnu/
+EOF
+mkdir -p /opt/amdgpu-pro/lib/x86_64-linux-gnu/
+# Grab a random lib
+cp /usr/lib/x86_64-linux-gnu/libz.so.1 /opt/amdgpu-pro/lib/x86_64-linux-gnu/libGL.so.1
 
 cd ../ros2_example_bazel_installed
 bazel build @ros2//:rviz2_rviz2
