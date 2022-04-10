6e3944c36e4dc7ed1427e8f9d6f07c06dcc7c0d6
Eric Cousineau <eric.cousineau@tri.global>
2022-04-10 16:24:06 -0400

    use singularity per Shane's instructions

--- repro/run_on_host.sh
+++ repro/run_on_host.sh
@@ -3,18 +3,8 @@ set -eux
 
 cd $(dirname ${BASH_SOURCE})
 
-deb=amdgpu-install_21.50.2.50002-1_all.deb
-
-if [[ ! -f ${deb} ]]; then
-    wget https://repo.radeon.com/amdgpu-install/21.50.2/ubuntu/focal/${deb}
-fi
-
-# Stupid gymnastics to ensure the entrypoint for this container isn't our
-# provisioning script (so it's easy to start and exec stuff).
-docker run -d -v ${PWD}/..:/opt/drake-ros ros:rolling-ros-base-focal sleep 100000
-id=$(docker ps -q | head -n 1)
-
-docker exec -it ${id} /opt/drake-ros/repro/run_on_container.sh
-
-# To clean up, can be heavy handed and remove *all* containers:
-#   docker ps -a -q | xargs docker rm -f
+singularity build --fakeroot repro.sif repro.def
+singularity exec \
+    --fakeroot --writable --no-home --containall \
+    --pwd /drake-ros/ros2_example_bazel_installed repro.sif \
+    bazel build @ros2//:builtin_interfaces_cc
