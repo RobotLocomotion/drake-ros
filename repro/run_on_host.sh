74895c26156bd7fc985730229671dd1f153ef9bf
Eric Cousineau <eric.cousineau@tri.global>
2022-04-10 16:43:13 -0400

    try caching; confused, but eh

--- repro/run_on_host.sh
+++ repro/run_on_host.sh
@@ -3,8 +3,26 @@ set -eux
 
 cd $(dirname ${BASH_SOURCE})
 
-singularity build --fakeroot repro.sif repro.def
+if [[ ! -f ./repro.sif ]]; then
+    singularity build --fakeroot repro.sif repro.def
+fi
+
+mkdir -p bazel_cache
+
+if [[ ! -f ./overlay.img ]]; then
+    dd if=/dev/zero of=overlay.img bs=1M count=500
+    mkfs.ext4 overlay.img
+fi
+
+# TODO(eric): How to make sandbox persistent?
+# I replaced `--writeable` with `--overlay ... --bind ...`, but eww.
 singularity exec \
-    --fakeroot --writable --no-home --containall \
-    --pwd /drake-ros/ros2_example_bazel_installed repro.sif \
+    --fakeroot \
+    --overlay ./overlay.img --bind ./bazel_cache:/root/.cache/bazel \
+    --no-home --containall \
+    --bind ..:/drake-ros \
+    --pwd /drake-ros/ros2_example_bazel_installed \
+    repro.sif \
     bazel build @ros2//:builtin_interfaces_cc
+
+    # bazel build //ros2_example_apps:oracle_cc
