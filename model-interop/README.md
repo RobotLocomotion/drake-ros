b10d35918bec0a327def8c966a3827ec69f60ef7
Marco A. Gutierrez <marco@openrobotics.org>
2022-04-29 12:10:20 +0000

    Model-interop inital prereqs, README and CI

---
+++ model-interop/README.md
@@ -0,0 +1,14 @@
+# Model interop
+
+## About
+
+This tool provides an automated workflow to import articulated robots (URDF / SDFormat) from other ecosystems into Drake.
+
+## Instructions
+
+First, install the required dependencies:
+
+```sh
+sudo ./setup/install_prereqs.sh
+```
+
---
+++ model-interop/setup/install_prereqs.sh
@@ -0,0 +1,8 @@
+apt-get update
+apt-get install lsb-release wget gnupg
+
+sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+apt-get update
+apt-get install ignition-edifice
+
