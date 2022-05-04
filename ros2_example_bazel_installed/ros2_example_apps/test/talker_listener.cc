82865b0c2f26940291aa455cdc19104a3168a277
Shane Loretz <sloretz@osrfoundation.org>
2022-05-03 18:30:34 -0700

    Add talker/listener example using rmw_isolation
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

---
+++ ros2_example_bazel_installed/ros2_example_apps/test/talker_listener.cc
@@ -0,0 +1,38 @@
+#include <chrono>
+#include <memory>
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+
+#include "rmw_isolation/rmw_isolation.h"
+
+#include "listener.hh"
+#include "talker.hh"
+
+
+int main(int argc, char* argv[]) {
+  const char* TEST_TMPDIR = std::getenv("TEST_TMPDIR");
+  if (TEST_TMPDIR != nullptr) {
+    ros2::isolate_rmw_by_path(argv[0], TEST_TMPDIR);
+  }
+  rclcpp::init(argc, argv);
+
+  auto talker = std::make_shared<Talker>();
+  auto listener = std::make_shared<Listener>();
+
+  rclcpp::executors::SingleThreadedExecutor exec;
+  exec.add_node(talker);
+  exec.add_node(listener);
+
+  auto got_message = listener->NextMessage();
+
+  auto result = 
+    exec.spin_until_future_complete(got_message, std::chrono::seconds(5));
+
+  rclcpp::shutdown();
+
+  if (result == rclcpp::FutureReturnCode::SUCCESS) {
+    return 0;
+  }
+  return 1;
+}
