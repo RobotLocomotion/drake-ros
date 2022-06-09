8065021eaaa60536eb2f8ddd881fbc5967f934da
Shane Loretz <sloretz@osrfoundation.org>
2022-06-09 15:58:03 -0700

    Top-level TEST_TMPDIR check

--- drake_ros_core/test/test_pub_sub.py
+++ drake_ros_core/test/test_pub_sub.py
@@ -30,13 +30,14 @@ import drake_ros_core
 from drake_ros_core import RosInterfaceSystem
 from drake_ros_core import RosPublisherSystem
 from drake_ros_core import RosSubscriberSystem
-from rmw_isolation import isolate_rmw_by_path
+
+
+if 'TEST_TMPDIR' in os.environ:
+    from rmw_isolation import isolate_rmw_by_path
+    isolate_rmw_by_path(os.environ['TEST_TMPDIR'])
 
 
 def test_nominal_case():
-    if 'TEST_TMPDIR' in os.environ:
-        isolate_rmw_by_path(os.environ['TEST_TMPDIR'])
-
     drake_ros_core.init()
 
     builder = DiagramBuilder()
