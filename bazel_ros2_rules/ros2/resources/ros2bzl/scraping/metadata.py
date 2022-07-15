9ed338214b3e333314b4c340a022867faafebadd
Shane Loretz <shane.loretz@gmail.com>
2022-07-14 17:27:37 -0700

    Fix scraping of plugin libraries from the ament index (#92)
    * Read plugin description paths from the ament index
    
    * missing colon
    
    * Typos, new line stripping, and absolute paths
    
    * Document pluginlib resource type perm link
    
    Signed-off-by: Shane Loretz <sloretz@openrobotics.org>

--- bazel_ros2_rules/ros2/resources/ros2bzl/scraping/metadata.py
+++ bazel_ros2_rules/ros2/resources/ros2bzl/scraping/metadata.py
@@ -87,6 +87,8 @@ def collect_ros_package_metadata(name, prefix):
     """
     share_directory = os.path.join(prefix, 'share', name)
     ament_index_directory = os.path.join(prefix, 'share', 'ament_index')
+    resource_index_directory = os.path.join(
+        ament_index_directory, 'resource_index')
 
     metadata = dict(
         prefix=prefix,
@@ -104,12 +106,27 @@ def collect_ros_package_metadata(name, prefix):
 
     metadata['langs'] = collect_package_langs(metadata)
 
-    path_to_plugins_description_xml = os.path.join(
-        share_directory, 'plugins_description.xml'
-    )
-    if os.path.exists(path_to_plugins_description_xml):
-        metadata.update(parse_plugins_description_xml(
-            path_to_plugins_description_xml
-        ))
+    # Find any plugins provided by this package
+    plugin_libraries = []
+    for resource_type in os.listdir(resource_index_directory):
+        # Pluginlib adds this suffix to the ament index resource type
+        # https://github.com/ros/pluginlib/blob/
+        # a11ecea28a587637d51f036e0e04eb194b94abfe/pluginlib/cmake/
+        # pluginlib_package_hook.cmake#L22
+        if resource_type.endswith('__pluginlib__plugin'):
+            plugin_resource = os.path.join(
+                resource_index_directory, resource_type, name)
+            if os.path.exists(plugin_resource):
+                with open(plugin_resource, 'r') as fin:
+                    path_to_desc = fin.read()
+                # Strip any trailing newline
+                path_to_desc = path_to_desc.strip()
+                if not os.path.isabs(path_to_desc):
+                    path_to_desc = os.path.join(prefix, path_to_desc)
+                if os.path.exists(path_to_desc):
+                    plugin_libraries.extend(parse_plugins_description_xml(
+                        path_to_desc)['plugin_libraries'])
+    if plugin_libraries:
+        metadata['plugin_libraries'] = plugin_libraries
 
     return metadata
