0946c52d1d80f29f19fc3c6ed2f92ec6aa122f91
Marco A. Gutierrez <marco@openrobotics.org>
2022-10-06 17:54:14 +0000

    adding xacro to install prereqs

--- drake_model_interop/setup/install_prereqs.sh
+++ drake_model_interop/setup/install_prereqs.sh
@@ -5,7 +5,7 @@ set -eux -o pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get install lsb-release wget gnupg python3 python3-venv python3-pyassimp git
+apt-get install lsb-release wget gnupg python3 python3-venv python3-pyassimp git ros-noetic-xacro
 
 wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/gazebo-stable.list
