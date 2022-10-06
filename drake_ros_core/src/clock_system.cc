2e6ebac51032c15683e18b0617d992101d08fb4d
Shane Loretz <sloretz@osrfoundation.org>
2022-10-06 14:35:42 -0700

    WIP clock system

---
+++ drake_ros_core/src/clock_system.cc
@@ -0,0 +1,25 @@
+#include "drake_ros_core/clock_system.h"
+
+#include <rclcpp/time.hpp>
+
+using drake_ros_core::ClockSystem;
+
+// class ClockSystem::Impl {
+// 
+// };
+
+ClockSystem::ClockSystem()
+{
+  DeclareAbstractOutputPort("clock", &ClockSystem::CalcClock);
+}
+
+ClockSystem::~ClockSystem()
+{
+}
+
+void ClockSystem::CalcClock(const drake::systems::Context<double>& context,
+                            rosgraph_msgs::msg::Clock* output_value) const {
+  rclcpp::Time now;
+  now += rclcpp::Duration::from_seconds(context.get_time());
+  output_value->clock = now;
+};
