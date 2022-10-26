RobotLocomotion/drake-ros

commit 041d7eb7aeae26c6d37a7dde7d0f85ecca45a97c

Use cmake-file-api instead of cmake-server (#150)
* Refactor dict to CcProperties to understand what's being used

* dict() -> PyProperties

* Use cmake-file-api instead of cmake-server

* Self-nits

* Docstr for CodeModel and Target

* Return type annotation

* Properties module docstr

bazel_ros2_rules/ros2/resources/ros2bzl/templates.py
@@ -63,9 +63,9 @@ def configure_package_cc_library(
     name, metadata, properties, dependencies, sandbox
 ):
     target_name = cc_name(name, metadata)
-    libraries = [sandbox(library) for library in properties['link_libraries']]
+    libraries = [sandbox(library) for library in properties.link_libraries]
     include_directories = [
-        sandbox(include) for include in properties['include_directories']]
+        sandbox(include) for include in properties.include_directories]
     local_includes = [
         include for include in include_directories
         if not os.path.isabs(include)]
@@ -81,11 +81,11 @@ def configure_package_cc_library(
         '-isystem ' + include
         for include in include_directories
         if os.path.isabs(include)]
-    copts.extend(properties['compile_flags'])
-    defines = properties['defines']
+    copts.extend(properties.compile_flags)
+    defines = properties.defines
 
-    linkopts = properties['link_flags']
-    for link_directory in properties['link_directories']:
+    linkopts = properties.link_flags
+    for link_directory in properties.link_directories:
         link_directory = sandbox(link_directory)
         if not link_directory:
             continue
@@ -149,9 +149,9 @@ def configure_package_py_library(
     name, metadata, properties, dependencies, sandbox
 ):
     target_name = py_name(name, metadata)
-    eggs = [sandbox(egg_path) for egg_path, _ in properties['python_packages']]
+    eggs = [sandbox(egg_path) for egg_path, _ in properties.python_packages]
     tops = [
-        sandbox(top_level) for _, top_level in properties['python_packages']]
+        sandbox(top_level) for _, top_level in properties.python_packages]
     imports = [os.path.dirname(egg) for egg in eggs]
 
     template = 'templates/package_py_library.bazel.tpl'
@@ -174,7 +174,7 @@ def configure_package_py_library(
     if 'cc' in metadata.get('langs', []):
         data.append(cc_label(name, metadata))
 
-    if 'cc_extensions' in properties:
+    if properties.cc_extensions:
         # Bring in C/C++ dependencies
         cc_deps = [
             cc_label(dependency_name, dependency_metadata)
@@ -182,12 +182,12 @@ def configure_package_py_library(
             if 'cc' in dependency_metadata.get('langs', [])
         ]
         # Expose C/C++ libraries if any
-        if 'cc_libraries' in properties:
+        if properties.cc_libraries:
             template = 'templates/package_py_library_with_cc_libs.bazel.tpl'
             config.update({
                 'cc_name': c_name("_" + target_name, metadata),
                 'cc_libs': [
-                    sandbox(lib) for lib in properties['cc_libraries']],
+                    sandbox(lib) for lib in properties.cc_libraries],
                 'cc_deps': cc_deps
             })
             data.append(c_label("_" + target_name, metadata))
@@ -195,7 +195,7 @@ def configure_package_py_library(
             data.extend(cc_deps)
         # Prepare runfiles to support dynamic loading
         cc_extensions = [
-            sandbox(extension) for extension in properties['cc_extensions']]
+            sandbox(extension) for extension in properties.cc_extensions]
         data.extend(cc_extensions)
 
     # Add in plugins, if any

bazel_ros2_rules/ros2/defs.bzl
@@ -9,8 +9,8 @@ COMMON_FILES_MANIFEST = [
     "rosidl.bzl",
     "_calculate_rosidl_capitalization.bzl",
     "resources/cmake_tools/__init__.py",
+    "resources/cmake_tools/file_api.py",
     "resources/cmake_tools/packages.py",
-    "resources/cmake_tools/server_mode.py",
     "resources/rmw_isolation/__init__.py",
     "resources/rmw_isolation/generate_isolated_rmw_env.py",
     "resources/rmw_isolation/package.BUILD.bazel",
@@ -29,6 +29,7 @@ COMMON_FILES_MANIFEST = [
     "resources/ros2bzl/scraping/ament_python.py",
     "resources/ros2bzl/scraping/__init__.py",
     "resources/ros2bzl/scraping/metadata.py",
+    "resources/ros2bzl/scraping/properties.py",
     "resources/ros2bzl/scraping/system.py",
     "resources/ros2bzl/templates.py",
     "resources/ros2bzl/utilities.py",

bazel_ros2_rules/ros2/generate_build_file.py
@@ -23,8 +23,6 @@ import ros2bzl.sandboxing as sandboxing
 
 from ros2bzl.scraping.ament_cmake \
     import collect_ament_cmake_package_direct_properties
-from ros2bzl.scraping.ament_cmake \
-    import collect_ament_cmake_package_properties
 from ros2bzl.scraping.ament_cmake import precache_ament_cmake_properties
 from ros2bzl.scraping.ament_python \
     import collect_ament_python_package_direct_properties

bazel_ros2_rules/ros2/resources/cmake_tools/__init__.py
@@ -7,7 +7,7 @@ import shutil
 import subprocess
 
 from .packages import get_packages_with_prefixes
-from .server_mode import server_mode
+from .file_api import get_cmake_codemodel
 
 
 def configure_file(src, dest, subs):

bazel_ros2_rules/ros2/resources/cmake_tools/file_api.py
@@ -1,0 +1,143 @@
+import json
+from pathlib import Path
+import subprocess
+from typing import List
+
+
+class Target:
+    """Represents a CMake target like a shared library or executable."""
+
+    __slots__ = (
+        'name',
+        'target_type',
+        'compile_flags',
+        'defines',
+        'link_flags',
+        'link_libraries',
+        'link_search_paths',
+        'includes',
+        'system_includes',
+    )
+
+    name: str
+    target_type: str
+    compile_flags: List[str]
+    defines: List[str]
+    link_flags: List[str]
+    link_libraries: List[str]
+    link_search_paths: List[str]
+    includes: List[str]
+    system_includes: List[str]
+
+    def __init__(self, target_file: Path):
+        assert target_file.is_file()
+        self.name = None
+        self.target_type = None
+        self.compile_flags = []
+        self.defines = []
+        self.link_flags = []
+        self.link_libraries = []
+        self.link_search_paths = []
+        self.includes = []
+        self.system_includes = []
+
+        target_data = json.loads(target_file.read_text())
+        self.name = target_data['name']
+        self.target_type = target_data['type']
+        if 'compileGroups' in target_data:
+            assert len(target_data['compileGroups']) == 1
+            compile_group = target_data['compileGroups'][0]
+
+            for fragment in compile_group['compileCommandFragments']:
+                self.compile_flags.append(fragment['fragment'])
+
+            for define in compile_group['defines']:
+                self.defines.append(define['define'])
+
+            if 'includes' in compile_group:
+                for include in compile_group['includes']:
+                    if 'isSystem' in include and include['isSystem']:
+                        self.system_includes.append(include['path'])
+                    else:
+                        self.includes.append(include['path'])
+
+        if 'link' in target_data:
+            link = target_data['link']
+            assert 'CXX' == link['language']
+
+            for fragment in link['commandFragments']:
+                if fragment['role'] == 'flags':
+                    self.link_flags.append(fragment['fragment'])
+                elif fragment['role'] == 'libraries':
+                    self.link_libraries.append(fragment['fragment'])
+                elif fragment['role'] == 'libraryPath':
+                    self.link_search_paths.append(fragment['fragment'])
+                elif fragment['role'] == 'frameworkPath':
+                    pass
+                else:
+                    raise RuntimeError(f'Unknown fragment role {fragment}')
+
+
+class CodeModel:
+    """Represents CMake's codemodel v2 from the file API."""
+
+    __slots__ = (
+        'targets',
+    )
+
+    targets: List[Target]
+
+    def __init__(self, codemodel_path: Path):
+        assert codemodel_path.is_file()
+
+        self.targets = []
+
+        codemodel = json.loads(codemodel_path.read_text())
+
+        assert len(codemodel['configurations']) == 1
+
+        cfg = codemodel['configurations'][0]
+        for target in cfg['targets']:
+            target_file = codemodel_path.parent.joinpath(target['jsonFile'])
+            self.targets.append(Target(target_file))
+
+
+def get_cmake_codemodel(project_path: Path, build_path: Path) -> CodeModel:
+    """
+    Use the cmake-file-api to get information about the code model of a CMake
+    project.
+
+    :param project_path: A path to a CMake project to analyze.
+    :param build_path: A path to a build directory for the project to analyze.
+    """
+    if not build_path.is_dir():
+        raise ValueError('The build_path directory must exist')
+    if not project_path.joinpath('CMakeLists.txt').is_file():
+        raise ValueError('The project_path must contain a CMakeLists.txt')
+
+    api_path = build_path.joinpath('.cmake/api/v1')
+    query_path = api_path.joinpath('query')
+    query_path.mkdir(parents=True)
+
+    # Make a shared stateless query for the codemodel
+    query_path.joinpath('codemodel-v2').touch()
+
+    args = ['cmake', str(project_path)]
+    subprocess.run(args, cwd=str(build_path))
+
+    # There should only be one file, but just in case the latest is the
+    # largest file in lexicographic order.
+    reply_path = api_path.joinpath('reply')
+    latest_reply = sorted(reply_path.glob('index-*.json'))[-1]
+
+    reply = json.loads(latest_reply.read_text())
+
+    codemodel_reply_file = None
+    for obj in reply['objects']:
+        if 'codemodel' == obj['kind']:
+            codemodel_reply_file = obj['jsonFile']
+
+    assert codemodel_reply_file
+    codemodel_reply_file = reply_path.joinpath(codemodel_reply_file)
+
+    return CodeModel(codemodel_reply_file)

bazel_ros2_rules/ros2/resources/cmake_tools/server_mode.py
@@ -1,195 +1,0 @@
-import contextlib
-import functools
-import json
-import os
-import re
-import selectors
-import signal
-import socket
-import subprocess
-import time
-import tempfile
-
-
-class CMakeServer:
-
-    START_MAGIC_STRING = '[== "CMake Server" ==['
-    END_MAGIC_STRING = ']== "CMake Server" ==]'
-
-    def __init__(self, address=None):
-        if not address:
-            address = tempfile.mktemp()
-        self._address = address
-        cmd = ['cmake', '-E', 'server', '--experimental']
-        cmd.append('--pipe=' + self._address)
-        self._process = subprocess.Popen(cmd)
-        while not os.path.exists(self._address):
-            if self._process.poll() is not None:
-                break
-            time.sleep(1)
-
-    @property
-    def address(self):
-        return self._address
-
-    def shutdown(self, timeout=None):
-        self._process.send_signal(signal.SIGINT)
-        try:
-            self._process.wait(timeout)
-        except subprocess.TimeoutExpired:
-            self._process.kill()
-
-
-class CMakeClient:
-
-    def __init__(self, address):
-        self._socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        try:
-            self._socket.connect(address)
-            self._selector = selectors.DefaultSelector()
-            self._selector.register(self._socket, selectors.EVENT_READ)
-            self._unpack_buffer = ''
-
-            message = next(self._receive(timeout=5), None)
-            assert message
-            assert message['type'] == 'hello'
-            self._supported_protocol_versions = \
-                message['supportedProtocolVersions']
-        except Exception:
-            self._socket.close()
-            raise
-
-    @property
-    def supported_protocol_versions(self):
-        return self._supported_protocol_versions
-
-    def shutdown(self):
-        self._socket.close()
-
-    def _pack(self, payload):
-        return '\n'.join([
-            CMakeServer.START_MAGIC_STRING,
-            json.dumps(payload),
-            CMakeServer.END_MAGIC_STRING
-        ]) + '\n'
-
-    def _unpack(self, partial):
-        self._unpack_buffer += partial
-        while self._unpack_buffer:
-            packet_start_index = \
-                self._unpack_buffer.find(CMakeServer.START_MAGIC_STRING)
-            if packet_start_index < 0:
-                break
-            payload_start_index = \
-                packet_start_index + len(CMakeServer.START_MAGIC_STRING)
-            payload_end_index = self._unpack_buffer.find(
-                CMakeServer.END_MAGIC_STRING, payload_start_index
-            )
-            if payload_end_index < 0:
-                break
-            payload = json.loads(
-                self._unpack_buffer[payload_start_index:payload_end_index]
-            )
-            packet_end_index = \
-                payload_end_index + len(CMakeServer.END_MAGIC_STRING)
-            self._unpack_buffer = self._unpack_buffer[packet_end_index:]
-
-            assert type(payload) is dict
-            assert 'type' in payload
-            yield payload
-
-    def _send(self, payload):
-        packet = self._pack(payload).encode('utf-8')
-        written = self._socket.send(packet)
-        return len(packet) == written
-
-    def _receive(self, timeout=None):
-        if timeout is not None:
-            deadline = time.time() + timeout
-        while True:
-            events = self._selector.select(timeout)
-            for key, mask in events:
-                assert key.fileobj is self._socket
-                assert mask & selectors.EVENT_READ
-                partial = self._socket.recv(4096)
-                yield from self._unpack(partial.decode('utf-8'))
-            if timeout is not None:
-                current_time = time.time()
-                if current_time >= deadline:
-                    break
-                timeout = deadline - current_time
-
-    def exchange(
-        self,
-        request,
-        message_callback=None,
-        progress_callback=None,
-        **kwargs
-    ):
-        assert self._send(request)
-        for response in self._receive(**kwargs):
-            if response['type'] == 'signal':
-                # Ignore signal responses
-                continue
-            if response['inReplyTo'] != request['type']:
-                continue
-            if response['type'] == 'reply':
-                return response
-            if response['type'] == 'error':
-                raise RuntimeError(response['errorMessage'])
-            if response['type'] == 'message':
-                if message_callback:
-                    message_callback(response['message'])
-                continue
-            if response['type'] == 'progress':
-                if progress_callback:
-                    message = response['progressMessage']
-                    progress = (
-                        response['progressMinimum'],
-                        response['progressMaximum'],
-                        response['progressCurrent']
-                    )
-                    progress_callback(message, progress)
-        raise TimeoutError(
-            'Timeout waiting for reply to {!r} request'.format(request)
-        )
-
-    def _request_method(self, type_, attributes=None, **kwargs):
-        request = {'type': type_}
-        if attributes:
-            request.update(attributes)
-        reply = self.exchange(request, **kwargs)
-        return {
-            k: v for k, v in reply.items()
-            # Drop transport specific attributes
-            if k not in ('inReplyTo', 'type', 'cookie')
-        }
-
-    def __getattr__(self, name):
-        return functools.partial(self._request_method, name)
-
-
-@contextlib.contextmanager
-def server_mode(project_path):
-    with tempfile.TemporaryDirectory() as tmpdir:
-        cmake_server = CMakeServer(address=os.path.join(tmpdir, 'pipe'))
-        try:
-            cmake = CMakeClient(cmake_server.address)
-            try:
-                supported_versions = cmake.supported_protocol_versions
-                assert any(v['major'] == 1 for v in supported_versions)
-                project_build_path = os.path.join(project_path, 'build')
-                cmake.handshake(
-                    attributes={
-                        'sourceDirectory': os.path.abspath(project_path),
-                        'buildDirectory': os.path.abspath(project_build_path),
-                        'generator': 'Unix Makefiles',
-                        'protocolVersion': {'major': 1}
-                    },
-                    timeout=5
-                )
-                yield cmake
-            finally:
-                cmake.shutdown()
-        finally:
-            cmake_server.shutdown()

commit bc702b57338c709566829f9277232c9f51187394

Ensure C/C++ Python libraries are included in scraped packages
This should resolve a regression where C/C++ libraries were dropped from
the file list for Python packages, resulting in import errors.

Fixes: 041d7eb7aeae26c6d37a7dde7d0f85ecca45a97c

bazel_ros2_rules/ros2/resources/ros2bzl/templates.py
@@ -174,7 +174,7 @@ def configure_package_py_library(
     if 'cc' in metadata.get('langs', []):
         data.append(cc_label(name, metadata))
 
-    if properties.cc_extensions:
+    if properties.cc_extensions or properties.cc_libraries:
         # Bring in C/C++ dependencies
         cc_deps = [
             cc_label(dependency_name, dependency_metadata)