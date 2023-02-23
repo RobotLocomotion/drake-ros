d0b378d962bd57550a8dd3fded3ca56eaf1af02e
Shane Loretz <sloretz@google.com>
2023-02-23 21:45:33 +0000

    Install sudo if necessary

--- .github/simplify_apt_and_upgrades.sh
+++ .github/simplify_apt_and_upgrades.sh
@@ -3,6 +3,13 @@ set -eux
 
 # Per discussion in https://github.com/RobotLocomotion/drake-ros/issues/221
 
+# Might not have sudo in a container
+if ! command -v sudo &> /dev/null
+then
+    apt-get update
+    apt-get install -y sudo
+fi
+
 # Disable interactive prompts from blocking CI runs.
 echo 'APT::Get::Assume-Yes "true";' | sudo tee /etc/apt/apt.conf.d/90yes
 echo "debconf debconf/frontend select Noninteractive" | sudo debconf-set-selections
