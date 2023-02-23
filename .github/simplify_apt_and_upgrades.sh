ddd87db046aa05ed2ea8309d6046e047a8dae447
Shane Loretz <sloretz@google.com>
2023-02-23 19:37:13 +0000

    Update package list before holding package

--- .github/simplify_apt_and_upgrades.sh
+++ .github/simplify_apt_and_upgrades.sh
@@ -8,4 +8,5 @@ echo 'APT::Get::Assume-Yes "true";' | sudo tee /etc/apt/apt.conf.d/90yes
 echo "debconf debconf/frontend select Noninteractive" | sudo debconf-set-selections
 
 # Prevent upgrades against packages that may interfere with testing.
+sudo apt-get update
 sudo apt-mark hold grub-efi-amd64-signed
