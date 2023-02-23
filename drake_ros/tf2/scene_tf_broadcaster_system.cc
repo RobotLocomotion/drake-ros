8968190d1eb11cd03a7a0c2c658575c3da41461f
Shane Loretz <sloretz@openrobotics.org>
2023-02-23 15:37:27 -0800

    Consolidate drake_ros_core, drake_ros_tf2, and drake_ros_viz packages (#205)
    - Replace action-ros-ci with manually calling colcon to avoid ros debian package
      installation when testing from archives
    - Add missing dep on visualization_msgs
    - Add step to install libconsole-bridge-dev
    
    Co-authored-by: Eric Cousineau <eric.cousineau@tri.global>

---
+++ drake_ros/tf2/scene_tf_broadcaster_system.cc
@@ -0,0 +1,74 @@
+// Copyright 2021 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "drake_ros/tf2/scene_tf_broadcaster_system.h"
+
+#include <memory>
+#include <unordered_set>
+
+#include <drake/systems/framework/diagram_builder.h>
+#include <drake_ros/core/drake_ros.h>
+#include <drake_ros/core/ros_publisher_system.h>
+#include <tf2_msgs/msg/tf_message.hpp>
+#include <tf2_ros/qos.hpp>
+
+#include "drake_ros/tf2/scene_tf_system.h"
+
+namespace drake_ros_tf2 {
+
+class SceneTfBroadcasterSystem::Impl {
+ public:
+  SceneTfSystem* scene_tf;
+  drake::systems::InputPortIndex graph_query_port_index;
+};
+
+SceneTfBroadcasterSystem::SceneTfBroadcasterSystem(
+    drake_ros_core::DrakeRos* ros, SceneTfBroadcasterParams params)
+    : impl_(new Impl()) {
+  drake::systems::DiagramBuilder<double> builder;
+
+  impl_->scene_tf = builder.AddSystem<SceneTfSystem>();
+
+  using drake_ros_core::RosPublisherSystem;
+  auto scene_tf_publisher =
+      builder.AddSystem(RosPublisherSystem::Make<tf2_msgs::msg::TFMessage>(
+          params.tf_topic_name, tf2_ros::DynamicBroadcasterQoS(), ros,
+          params.publish_triggers, params.publish_period));
+
+  builder.Connect(impl_->scene_tf->get_scene_tf_output_port(),
+                  scene_tf_publisher->get_input_port());
+
+  impl_->graph_query_port_index = builder.ExportInput(
+      impl_->scene_tf->get_graph_query_input_port(), "graph_query");
+
+  builder.BuildInto(this);
+}
+
+SceneTfBroadcasterSystem::~SceneTfBroadcasterSystem() {}
+
+void SceneTfBroadcasterSystem::RegisterMultibodyPlant(
+    const drake::multibody::MultibodyPlant<double>* plant) {
+  impl_->scene_tf->RegisterMultibodyPlant(plant);
+}
+
+void SceneTfBroadcasterSystem::ComputeFrameHierarchy() {
+  impl_->scene_tf->ComputeFrameHierarchy();
+}
+
+const drake::systems::InputPort<double>&
+SceneTfBroadcasterSystem::get_graph_query_input_port() const {
+  return get_input_port(impl_->graph_query_port_index);
+}
+
+}  // namespace drake_ros_tf2
---
+++ drake_ros/tf2/test/test_name_conventions.cc
@@ -0,0 +1,97 @@
+// Copyright 2022 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+
+#include "internal_name_conventions.h"  // NOLINT
+#include <gtest/gtest.h>
+
+#include "drake_ros/tf2/name_conventions.h"
+
+using drake::geometry::FrameId;
+using drake::multibody::ModelInstanceIndex;
+using drake::multibody::MultibodyPlant;
+using drake::systems::DiagramBuilder;
+
+TEST(NameConventions, CalcTfFrameNameWithBody) {
+  // All names are present
+  EXPECT_EQ("model_name/body_name/456",
+            drake_ros_tf2::internal::CalcTfFrameName<int>(
+                "model_name", "body_name", 123, 456));
+
+  // Scoped model name and body name
+  EXPECT_EQ("model_scope/model_name/body_scope/body_name/456",
+            drake_ros_tf2::internal::CalcTfFrameName(
+                "model_scope::model_name", "body_scope::body_name", 123, 456));
+
+  // Model name with empty body name
+  EXPECT_EQ(
+      "model_name/unnamed_body_123/456",
+      drake_ros_tf2::internal::CalcTfFrameName("model_name", "", 123, 456));
+
+  // Scoped model name with empty body name
+  EXPECT_EQ("model_scope/model_name/unnamed_body_123/456",
+            drake_ros_tf2::internal::CalcTfFrameName("model_scope::model_name",
+                                                     "", 123, 456));
+}
+
+TEST(NameConventions, CalcTfFrameNameWithoutBody) {
+  // Frame name is not empty
+  EXPECT_EQ("frame_name",
+            drake_ros_tf2::internal::CalcTfFrameName("frame_name", 123));
+
+  // Frame name is empty
+  EXPECT_EQ("unnamed_frame_123",
+            drake_ros_tf2::internal::CalcTfFrameName("", 123));
+
+  // Frame name with / delimiters
+  EXPECT_EQ("scope_one/scope_two/frame_name",
+            drake_ros_tf2::internal::CalcTfFrameName(
+                "scope_one/scope_two/frame_name", 123));
+
+  // Frame name with :: delimiters
+  EXPECT_EQ("scope_one/scope_two/frame_name",
+            drake_ros_tf2::internal::CalcTfFrameName(
+                "scope_one::scope_two::frame_name", 123));
+
+  // Frame name with mixed delimiters
+  EXPECT_EQ("scope_one/scope_two/frame_name",
+            drake_ros_tf2::internal::CalcTfFrameName(
+                "scope_one/scope_two::frame_name", 123));
+
+  // Frame name with only :: delimiter
+  EXPECT_EQ("unnamed_frame_123",
+            drake_ros_tf2::internal::CalcTfFrameName("::", 123));
+
+  // Frame name with only / delimiter
+  EXPECT_EQ("unnamed_frame_123",
+            drake_ros_tf2::internal::CalcTfFrameName("/", 123));
+}
+
+TEST(NameConventions, GetTfFrameNameWorld) {
+  DiagramBuilder<double> builder;
+
+  auto [plant, scene_graph] = AddMultibodyPlantSceneGraph(
+      &builder, std::make_unique<MultibodyPlant<double>>(0.0));
+
+  std::unordered_set<const MultibodyPlant<double>*> plants = {&plant};
+
+  // World frame is always ModelInstanceIndex 0
+  const auto world_model_index = ModelInstanceIndex{0};
+  FrameId frame_id = plant.GetBodyFrameIdOrThrow(
+      plant.GetBodyIndices(world_model_index).at(0));
+
+  EXPECT_EQ("world", drake_ros_tf2::GetTfFrameName(
+                         scene_graph.model_inspector(), plants, frame_id));
+}
