f90f0febe195a21164454d77e923884acd02d8b6
Eric Cousineau <eric.cousineau@tri.global>
2023-03-07 17:23:45 -0500

    [py] Avoid ODR in bindings (#239)
    - Offer roll-up static library `:drake_ros` and shared library,
      `:drake_ros_shared_library` (using already compiled object files)
    - Use shared library variant for Python bindings
    - Add Bazel-only ODR violation test
    - Add DRAKE_ROS_NO_EXPORT for Python bindings
    - Disable clang-format around drake_ros_py namespace comments
    
    Co-authored-by: Shane Loretz <sloretz@openrobotics.org>

--- drake_ros/drake_ros/viz/cc_pybind.cc
+++ drake_ros/drake_ros/viz/cc_pybind.cc
@@ -9,7 +9,7 @@
 #include "drake_ros/viz/rviz_visualizer.h"
 
 namespace drake_ros {
-namespace drake_ros_py {
+namespace drake_ros_py DRAKE_ROS_NO_EXPORT {
 
 using drake_ros_core::DrakeRos;
 using drake_ros_viz::RvizVisualizer;
@@ -52,5 +52,7 @@ void DefViz(py::module m) {
            py::return_value_policy::reference_internal);
 }
 
+// clang-format off
 }  // namespace drake_ros_py
+// clang-format on
 }  // namespace drake_ros
