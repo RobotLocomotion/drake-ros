ce81a8dbfb4e8a7b24777e2b215ccfda06dffa97
Shane Loretz <sloretz@google.com>
2023-03-10 19:58:45 +0000

    Remove unused stuff

--- drake_ros_examples/examples/hydroelastic/hydroelastic.cc
+++ drake_ros_examples/examples/hydroelastic/hydroelastic.cc
@@ -8,33 +8,18 @@
 
 #include "drake/multibody/meshcat/contact_visualizer.h"
 #include <ament_index_cpp/get_package_share_directory.hpp>
-#include <drake/common/value.h>
-#include <drake/geometry/geometry_frame.h>
-#include <drake/geometry/geometry_ids.h>
-#include <drake/geometry/geometry_instance.h>
-#include <drake/geometry/geometry_roles.h>
 #include <drake/geometry/kinematics_vector.h>
 #include <drake/geometry/meshcat.h>
 #include <drake/geometry/meshcat_visualizer.h>
-#include <drake/geometry/proximity_properties.h>
-#include <drake/geometry/query_object.h>
-#include <drake/geometry/query_results/contact_surface.h>
 #include <drake/geometry/scene_graph.h>
-#include <drake/geometry/shape_specification.h>
-#include <drake/lcm/drake_lcm.h>
-#include <drake/lcmt_contact_results_for_viz.hpp>
 #include <drake/math/rigid_transform.h>
 #include <drake/multibody/parsing/parser.h>
-#include <drake/multibody/plant/contact_results_to_lcm.h>
 #include <drake/multibody/plant/multibody_plant.h>
 #include <drake/systems/analysis/simulator.h>
 #include <drake/systems/analysis/simulator_config.h>
 #include <drake/systems/analysis/simulator_config_functions.h>
 #include <drake/systems/framework/context.h>
-#include <drake/systems/framework/continuous_state.h>
 #include <drake/systems/framework/diagram_builder.h>
-#include <drake/systems/framework/leaf_system.h>
-#include <drake/systems/lcm/lcm_publisher_system.h>
 #include <drake_ros/core/drake_ros.h>
 #include <drake_ros/core/ros_interface_system.h>
 #include <drake_ros/core/ros_publisher_system.h>
@@ -51,25 +36,12 @@
 using bazel::tools::cpp::runfiles::Runfiles;
 #endif
 
-using drake::geometry::AddCompliantHydroelasticProperties;
-using drake::geometry::AddContactMaterial;
-using drake::geometry::AddRigidHydroelasticProperties;
-using drake::geometry::HalfSpace;
 using drake::geometry::Meshcat;
 using drake::geometry::MeshcatVisualizerd;
 using drake::geometry::MeshcatVisualizerParams;
-using drake::geometry::ProximityProperties;
-using drake::geometry::Sphere;
-using drake::math::RigidTransformd;
-using drake::math::RollPitchYawd;
-using drake::math::RotationMatrixd;
 using drake::multibody::AddMultibodyPlantSceneGraph;
 using drake::multibody::ContactModel;
-using drake::multibody::CoulombFriction;
 using drake::multibody::Parser;
-using drake::multibody::RigidBody;
-using drake::multibody::SpatialInertia;
-using drake::multibody::UnitInertia;
 using drake::multibody::meshcat::ContactVisualizerd;
 using drake::multibody::meshcat::ContactVisualizerParams;
 using drake::systems::ApplySimulatorConfig;
@@ -82,16 +54,12 @@ using drake_ros_core::RosInterfaceSystem;
 using drake_ros_viz::ConnectContactResultsToRviz;
 using drake_ros_viz::ContactMarkersParams;
 using drake_ros_viz::RvizVisualizer;
-using Eigen::Vector3d;
-using Eigen::Vector4d;
-using std::make_unique;
 
 using MultibodyPlantd = drake::multibody::MultibodyPlant<double>;
 
 DEFINE_double(simulation_sec, std::numeric_limits<double>::infinity(),
               "How many seconds to run the simulation");
 DEFINE_bool(real_time, true, "Set to false to run as fast as possible");
-
 DEFINE_bool(use_meshcat, false, "Enable meshcat visualizer.");
 
 namespace drake_ros_examples {
