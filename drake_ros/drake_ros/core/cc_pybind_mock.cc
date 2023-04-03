a5b3882ecbf6e1617711de27681994acf80bff00
Aditya Pande <adityapande@google.com>
2023-04-03 23:17:21 +0000

    clang-format
    Signed-off-by: Aditya Pande <adityapande@google.com>

--- drake_ros/drake_ros/core/cc_pybind_mock.cc
+++ drake_ros/drake_ros/core/cc_pybind_mock.cc
@@ -1,3 +1,4 @@
+#include "geometry_msgs/msg/polygon.hpp"
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
@@ -5,26 +6,24 @@
 #include "drake_ros/core/ros_idl_pybind.h"
 #include "drake_ros/drake_ros_pybind.h"
 
-#include "geometry_msgs/msg/polygon.hpp"
-
 // This is meant to be a test module, to just test
 // the generic typecaster macro.
 namespace PYBIND11_NAMESPACE {
 namespace detail {
-  ROS_MSG_PYBIND_TYPECAST_ALL();
+ROS_MSG_PYBIND_TYPECAST_ALL();
 }
-}
+}  // namespace PYBIND11_NAMESPACE
 
 namespace drake_ros {
 namespace mock_test {
-  template <typename T>
-  T testTypecasting(T msg) {
-    std::cout << "Message type: " <<
-      rosidl_generator_traits::name<T>() << std::endl;
-    return msg;
-  }
+template <typename T>
+T testTypecasting(T msg) {
+  std::cout << "Message type: " << rosidl_generator_traits::name<T>()
+            << std::endl;
+  return msg;
 }
-}
+}  // namespace mock_test
+}  // namespace drake_ros
 
 namespace drake_ros {
 namespace drake_ros_py DRAKE_ROS_NO_EXPORT {
@@ -52,11 +51,11 @@ void DefTestTypecasting(py::module m) {
   m.def("shutdown", &shutdown);
 
   m.def("testTypecasting",
-      &drake_ros::mock_test::testTypecasting<geometry_msgs::msg::Polygon>,
-      py::arg("msg"));
+        &drake_ros::mock_test::testTypecasting<geometry_msgs::msg::Polygon>,
+        py::arg("msg"));
   m.def("testTypecasting",
-      &drake_ros::mock_test::testTypecasting<geometry_msgs::msg::Quaternion>,
-      py::arg("msg"));
+        &drake_ros::mock_test::testTypecasting<geometry_msgs::msg::Quaternion>,
+        py::arg("msg"));
 }
 // clang-format off
 }  // namespace drake_ros_py
