84ad68a10dd119d10e4f950d931993bf5065c384
Eric Cousineau <eric.cousineau@tri.global>
2023-05-11 12:36:07 -0400

    narrow scope of aliases

--- drake_ros/drake_ros/core/qos_pybind.h
+++ drake_ros/drake_ros/core/qos_pybind.h
@@ -20,28 +20,34 @@ class QoS : public rclcpp::QoS {
 
 namespace drake_ros {
 namespace drake_ros_py {
-using std::enable_if_t;
-using std::remove_cv_t;
-using handle = py::handle;
-using object = py::object;
-using return_value_policy = py::return_value_policy;
-using none = py::none;
+
 // TODO(hidmic): rely on rclpy.qos.QoSProfile when and if a pybind11 binding
 // is introduced upstream. See https://github.com/ros2/rclpy/issues/843.
 template <typename T>
 struct qos_type_caster {
  public:
+  // N.B. These aliases are to appease the PYBIND11_TYPE_CASTER macro in
+  // Drake's fork of pybind11.
+  template <bool Cond, typename U = void>
+  using enable_if_t = std::enable_if_t<Cond, U>;
+  template <typename U>
+  using remove_cv_t = std::remove_cv_t<U>;
+  using object = py::handle;
+  using handle = py::handle;
+  using return_value_policy = py::return_value_policy;
+  using none = py::none;
+
   // declares local 'value' of type QoS
   PYBIND11_TYPE_CASTER(drake_ros::QoS, py::detail::_("rclpy.qos.QoSProfile"));
 
   // Convert from Python rclpy.qos.QoSProfile to QoS
-  bool load(handle src, bool) {
-    handle cls = py::module::import("rclpy.qos").attr("QoSProfile");
-    if (!isinstance(src, cls)) {
+  bool load(py::handle src, bool) {
+    py::handle cls = py::module::import("rclpy.qos").attr("QoSProfile");
+    if (!py::isinstance(src, cls)) {
       return false;
     }
 
-    object source = py::reinterpret_borrow<object>(src);
+    py::object source = py::reinterpret_borrow<py::object>(src);
     // history                          : enum int
     // depth                            : int
     // reliability                      : enum int
@@ -86,44 +92,44 @@ struct qos_type_caster {
   }
 
   // Convert from Python QoS to rclpy.qos.QoSProfile
-  static handle cast(drake_ros::QoS src, return_value_policy policy,
-                     handle parent) {
+  static py::handle cast(
+      drake_ros::QoS src, py::return_value_policy policy, py::handle parent) {
     (void)policy;
     (void)parent;
 
     const auto& rmw_qos = src.get_rmw_qos_profile();
 
-    object duration = py::module::import("rclpy.duration").attr("Duration");
+    py::object duration = py::module::import("rclpy.duration").attr("Duration");
 
-    object lifespan_duration =
-        duration(pybind11::arg("seconds") = rmw_qos.lifespan.sec,
-                 pybind11::arg("nanoseconds") = rmw_qos.lifespan.nsec);
+    py::object lifespan_duration =
+        duration(py::arg("seconds") = rmw_qos.lifespan.sec,
+                 py::arg("nanoseconds") = rmw_qos.lifespan.nsec);
 
-    object deadline_duration =
-        duration(pybind11::arg("seconds") = rmw_qos.deadline.sec,
-                 pybind11::arg("nanoseconds") = rmw_qos.deadline.nsec);
+    py::object deadline_duration =
+        duration(py::arg("seconds") = rmw_qos.deadline.sec,
+                 py::arg("nanoseconds") = rmw_qos.deadline.nsec);
 
-    object liveliness_lease_duration = duration(
-        pybind11::arg("seconds") = rmw_qos.liveliness_lease_duration.sec,
-        pybind11::arg("nanoseconds") = rmw_qos.liveliness_lease_duration.nsec);
+    py::object liveliness_lease_duration = duration(
+        py::arg("seconds") = rmw_qos.liveliness_lease_duration.sec,
+        py::arg("nanoseconds") = rmw_qos.liveliness_lease_duration.nsec);
 
-    object instance =
+    py::object instance =
         py::module::import("rclpy.qos")
             .attr("QoSProfile")(
-                pybind11::arg("history") =
+                py::arg("history") =
                     static_cast<ssize_t>(rmw_qos.history),
-                pybind11::arg("depth") = static_cast<size_t>(rmw_qos.depth),
-                pybind11::arg("reliability") =
+                py::arg("depth") = static_cast<size_t>(rmw_qos.depth),
+                py::arg("reliability") =
                     static_cast<ssize_t>(rmw_qos.reliability),
-                pybind11::arg("durability") =
+                py::arg("durability") =
                     static_cast<ssize_t>(rmw_qos.durability),
-                pybind11::arg("lifespan") = lifespan_duration,
-                pybind11::arg("deadline") = deadline_duration,
-                pybind11::arg("liveliness") =
+                py::arg("lifespan") = lifespan_duration,
+                py::arg("deadline") = deadline_duration,
+                py::arg("liveliness") =
                     static_cast<ssize_t>(rmw_qos.liveliness),
-                pybind11::arg("liveliness_lease_duration") =
+                py::arg("liveliness_lease_duration") =
                     liveliness_lease_duration,
-                pybind11::arg("avoid_ros_namespace_conventions") =
+                py::arg("avoid_ros_namespace_conventions") =
                     rmw_qos.avoid_ros_namespace_conventions);
     instance.inc_ref();
     return instance;
