bcdd80b2a1f6494d54ec996fee03cd4f47ffb4fe
Aditya Pande <adityapande@google.com>
2023-08-21 14:39:29 +0000

    WIP: Support for ros launch with a new rule

---
+++ ros2_example_bazel_installed/ros2_example_apps/eg_launch.py
@@ -0,0 +1,10 @@
+from launch import LaunchDescription
+from roslaunch_util import ExecuteBazelTarget
+
+def generate_launch_description():
+    return LaunchDescription([
+        # Running a talker written in python.
+        ExecuteBazelTarget('eg_talker'),
+        # Running a listener written in cpp.
+        ExecuteBazelTarget('eg_listener'),
+    ])
