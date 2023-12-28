8a7ff0059290d0e35f1a347ac373442c01cf40b5
Alejandro Hernández Cordero <ahcorde@gmail.com>
2023-12-28 12:03:34 +0100

    Fix Ci
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- ros2_example_bazel_installed/setup/install_prereqs.sh
+++ ros2_example_bazel_installed/setup/install_prereqs.sh
@@ -76,7 +76,7 @@ if [[ -z "${ROS2_DISTRO_PREFIX:-}" ]]; then
   add-apt-repository universe
 
   apt update && apt install curl -y
-  curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+  curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg add -
 
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" |  tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
