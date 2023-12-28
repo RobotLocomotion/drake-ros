8d0eb2af3ca3ed82119dcfcab898acfb543bfdd6
Alejandro Hernández Cordero <ahcorde@gmail.com>
2023-12-28 12:10:20 +0100

    Fix Ci
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- ros2_example_bazel_installed/setup/install_prereqs.sh
+++ ros2_example_bazel_installed/setup/install_prereqs.sh
@@ -75,10 +75,10 @@ if [[ -z "${ROS2_DISTRO_PREFIX:-}" ]]; then
   apt install software-properties-common
   add-apt-repository universe
 
-  apt update && apt install curl -y
-  curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | apt-key --keyring /usr/share/keyrings/ros-archive-keyring.gpg add -
+  # apt update && apt install curl -y
+  # curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | apt-key --keyring /usr/share/keyrings/ros-archive-keyring.gpg add -
 
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" |  tee /etc/apt/sources.list.d/ros2.list > /dev/null
+  # echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" |  tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
   apt update
 
