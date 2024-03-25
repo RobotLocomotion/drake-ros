d000c8b193f2e95e2f8e9d2dacdbd249c224b2fc
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-03-25 18:31:05 +0100

    make linters happy
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- bazel_ros2_rules/network_isolation/network_isolation_py.cc
+++ bazel_ros2_rules/network_isolation/network_isolation_py.cc
@@ -1,11 +1,12 @@
-#include "network_isolation/network_isolation.h"
-
 #include <pybind11/pybind11.h>
+
+#include "network_isolation/network_isolation.h"
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(network_isolation_py, m) {
-    m.def("create_linux_namespaces", &network_isolation::create_linux_namespaces, R"pbdoc(
+PYBIND11_MODULE(network_isolation_py, m)
+{
+  m.def("create_linux_namespaces", &network_isolation::create_linux_namespaces, R"pbdoc(
         Creates linux namespaces suitable for isolating ROS 2 traffic.
     )pbdoc");
 }
