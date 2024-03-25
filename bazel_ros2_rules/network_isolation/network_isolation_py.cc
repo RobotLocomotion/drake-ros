c2b9823720a495ec2e718810e3b3e73ee3d343a8
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-03-25 18:29:13 +0100

    suggestions to network isolation
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- bazel_ros2_rules/network_isolation/network_isolation_py.cc
+++ bazel_ros2_rules/network_isolation/network_isolation_py.cc
@@ -1,35 +1,11 @@
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
-
 #include "network_isolation/network_isolation.h"
 
-extern "C" {
-static PyObject *
-create_linux_namespaces(PyObject *, PyObject *)
-{
-    if (!network_isolation::create_linux_namespaces()) {
-        Py_RETURN_FALSE;
-    }
-    Py_RETURN_TRUE;
-}
+#include <pybind11/pybind11.h>
 
-static PyMethodDef methods[] = {
-    {"create_linux_namespaces", &create_linux_namespaces, METH_NOARGS,
-     "Isolate the current process using linux namespaces."},
-    {NULL, NULL, 0, NULL}   /* sentinel */
-};
+namespace py = pybind11;
 
-static PyModuleDef module = {
-    PyModuleDef_HEAD_INIT,
-    .m_name = "network_isolation_py",
-    .m_doc = "Tools to isolate network traffic on linux.",
-    .m_size = -1,
-    methods,
-};
-
-PyMODINIT_FUNC
-PyInit_network_isolation_py(void)
-{
-    return PyModule_Create(&module);
-}
+PYBIND11_MODULE(network_isolation_py, m) {
+    m.def("create_linux_namespaces", &network_isolation::create_linux_namespaces, R"pbdoc(
+        Creates linux namespaces suitable for isolating ROS 2 traffic.
+    )pbdoc");
 }
