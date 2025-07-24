51dfa795adfaa7cdeb4e301550b358222192891a
Francisco <francisco.rossi.ctr@tri.global>
2025-07-24 18:11:41 -0300

    Bring apt update and ros-dev-tools back

--- drake_ros_examples/setup/install_prereqs.sh
+++ drake_ros_examples/setup/install_prereqs.sh
@@ -24,6 +24,8 @@ export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrast
 curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb" # If using Ubuntu derivates use $UBUNTU_CODENAME
 sudo dpkg -i /tmp/ros2-apt-source.deb
 
+sudo apt update && sudo apt install ros-dev-tools
+
 # End - ROS Installation
 
 apt install ros-dev-tools ros-jazzy-rmw-cyclonedds-cpp ros-jazzy-rviz2 ros-jazzy-ros2cli-common-extensions
