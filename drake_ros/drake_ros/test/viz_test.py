e568e5193800a4fd22fd7cd723a1572e371d26ae
Francisco <francisco.rossi.ctr@tri.global>
2025-07-24 18:25:38 -0300

    Remove instances of deprecated manipulation station

--- drake_ros/drake_ros/test/viz_test.py
+++ drake_ros/drake_ros/test/viz_test.py
@@ -12,7 +12,6 @@ import rclpy.executors
 import rclpy.node
 from visualization_msgs.msg import Marker, MarkerArray
 
-from pydrake.examples import ManipulationStation
 from pydrake.systems.analysis import Simulator
 from pydrake.systems.framework import DiagramBuilder
 from pydrake.systems.primitives import ConstantVectorSource
@@ -100,84 +99,6 @@ class ManagedSubscription:
 
     def spin_complete(self):
         return self._spin_complete.is_set()
-
-
-class DrakeTestSystem:
-    def __init__(self):
-        drake_ros.core.init()
-
-        builder = DiagramBuilder()
-        ros_interface_system = builder.AddSystem(
-            RosInterfaceSystem('drake_ros_viz_test'))
-
-        self.manipulation_station = builder.AddSystem(ManipulationStation())
-        self.manipulation_station.SetupClutterClearingStation()
-        self.manipulation_station.Finalize()
-
-        constant_term = builder.AddSystem(ConstantVectorSource(
-            np.zeros(self.manipulation_station.num_iiwa_joints())))
-        builder.Connect(
-            constant_term.get_output_port(),
-            self.manipulation_station.GetInputPort('iiwa_position'))
-
-        rviz_visualizer = builder.AddSystem(
-            RvizVisualizer(ros_interface_system.get_ros_interface()))
-        rviz_visualizer.RegisterMultibodyPlant(
-            self.manipulation_station.get_multibody_plant())
-        builder.Connect(
-            self.manipulation_station.GetOutputPort('query_object'),
-            rviz_visualizer.get_graph_query_input_port()
-        )
-
-        self.diagram = builder.Build()
-
-        self.simulator = Simulator(self.diagram)
-        self.simulator.Initialize()
-        self.simulator.set_target_realtime_rate(1.0)
-        self.context = self.simulator.get_mutable_context()
-        manipulation_station_context = self.diagram.GetMutableSubsystemContext(
-            self.manipulation_station,
-            self.context)
-        self.manipulation_station.GetInputPort('wsg_position').FixValue(
-            manipulation_station_context, np.zeros(1))
-
-    def advance(self):
-        self.simulator.AdvanceTo(self.context.get_time() + 0.1)
-
-
-def test_receive_visual_marker_array():
-    with ManagedSubscription(required_message_count=2) as managed_subscription:
-        drake_test_system = DrakeTestSystem()
-
-        managed_subscription.spin_subscription(timeout=10)
-        try:
-            while not managed_subscription.spin_complete():
-                drake_test_system.advance()
-        except KeyboardInterrupt:
-            pass
-        rx_messages = managed_subscription.wait_for_and_get_received_messages()
-
-        # Get at least two messages to confirm the markers are being updated
-        assert len(rx_messages) >= 2
-        test_message = rx_messages[0]
-        test_markers = test_message.markers
-
-        # Make sure there are some markers in the array
-        assert len(test_markers) >= 2
-
-        # Do some very simple (but flexible) checks on the markers
-        types = set(marker.type for marker in test_markers)
-        assert types >= {Marker.ADD, Marker.CUBE, Marker.MESH_RESOURCE}
-        namespaces = set(marker.ns for marker in test_markers)
-        assert "" in namespaces
-        mesh_resources = set(
-            marker.mesh_resource
-            for marker in test_markers
-            if marker.type == marker.MESH_RESOURCE
-        )
-        assert len(mesh_resources) >= 1
-        assert "" not in mesh_resources
-
 
 if __name__ == '__main__':
     isolate_if_using_bazel()
