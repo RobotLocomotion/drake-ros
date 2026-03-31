cc2224efd97a01a80af290ee003ccb3b7b5b2b07
julian-heine-ctr <julian.heine.ctr@tri.global>
2026-03-31 17:05:55 -0300

    Add idl support to rosidl rules

---
+++ ros2_example_bazel_installed/ros2_example_apps/test/policy_msgs_test.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <memory>
+
+#include "policy_msgs/srv/policy_reset.hpp"
+
+int main() {
+  auto request = std::make_shared<policy_msgs::srv::PolicyReset::Request>();
+  request->seed = 12;
+  std::cout << "Successfully instantiated PolicyReset with seed: "
+            << request->seed << std::endl;
+  return 0;
+}
